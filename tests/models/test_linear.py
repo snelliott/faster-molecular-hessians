fcd1052f6a3f2ad6aa62bdb2b77d7fc4745fc398
Logan Ward <ward.logan.t@gmail.com>
2023-08-30 08:29:44 -0400

    Refactor and add linear terms
    Fixes #2
    Fixes #4

--- tests/models/test_linear.py
+++ tests/models/test_linear.py
@@ -2,7 +2,7 @@ import numpy as np
 from ase.build import molecule
 from ase.vibrations import VibrationsData
 
-from jitterbug.model.linear import get_displacement_matrix, LinearHessianModel
+from jitterbug.model.linear import get_model_inputs, HarmonicModel
 
 
 def test_disp_matrix():
@@ -11,18 +11,20 @@ def test_disp_matrix():
 
     # With a single displacement only the first term should be nonzero
     atoms.positions[0, 0] += 0.1
-    disp_matrix = get_displacement_matrix(atoms, reference)
-    assert disp_matrix.size == 21
-    assert (disp_matrix != 0).sum() == 1
-    assert np.isclose(disp_matrix[0], 0.01)
+    disp_matrix = get_model_inputs(atoms, reference)
+    assert disp_matrix.size == 27  # 6 linear, 21 harmonic terms
+    assert (disp_matrix != 0).sum() == 2  # One linear and one harmonic term
+    assert np.isclose(disp_matrix[0], 0.1)  # Linear terms
+    assert np.isclose(disp_matrix[6], 0.01)  # Harmonic terms
 
     # With two displacements, there should be 3 nonzero terms
     atoms.positions[1, 0] += 0.05
-    disp_matrix = get_displacement_matrix(atoms, reference)
-    assert (disp_matrix != 0).sum() == 3
-    assert np.isclose(disp_matrix[0], 0.01)  # (Atom 0, x) * (Atom 0, x)
-    assert np.isclose(disp_matrix[3], 0.1 * 0.05 * 2)  # (Atom 0, x) * (Atom 1, x) * 2 (harmonic)
-    assert np.isclose(disp_matrix[6 + 5 + 4], 0.0025)  # (Atom 1, x) * (Atom 1, x)
+    disp_matrix = get_model_inputs(atoms, reference)
+    assert (disp_matrix != 0).sum() == 2 + 3
+    assert np.isclose(disp_matrix[[0, 3]], [0.1, 0.05]).all()  # Linear terms
+    assert np.isclose(disp_matrix[6], 0.01)  # (Atom 0, x) * (Atom 0, x)
+    assert np.isclose(disp_matrix[6 + 3], 0.1 * 0.05 * 2)  # (Atom 0, x) * (Atom 1, x) * 2 (harmonic)
+    assert np.isclose(disp_matrix[6 + 6 + 5 + 4], 0.0025)  # (Atom 1, x) * (Atom 1, x)
 
 
 def test_linear_model(train_set):
@@ -31,12 +33,17 @@ def test_linear_model(train_set):
     assert reference.get_forces().max() < 0.01
 
     # Fit the model
-    model = LinearHessianModel(reference)
+    model = HarmonicModel(reference)
     hessian_model = model.train(train_set)
+    assert hessian_model.coef_.shape == (54,)
+
+    # Get the mean hessian
+    hessian = model.mean_hessian(hessian_model)
+    assert hessian.shape == (9, 9)
 
     # Sample the Hessians, at least make sure the results are near correct
     hessians = model.sample_hessians(hessian_model, num_samples=32)
-    assert len(hessians)
+    assert len(hessians) == 32
     assert np.isclose(hessians[0], hessians[0].T).all()
 
     # Create a vibration data object
